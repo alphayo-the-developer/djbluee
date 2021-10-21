8df99fee23dc9e402418ec9e117ec4599bf2a5b6
alphayo-the-developer <alphayothedeveloper@gmail.com>
2021-10-21 16:23:02 +0300

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,40 @@
+// MENU SHOW 
+const showMenu = (toggleId, navId)=>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+    var visibility = nav.style.display;
+    console.log(visibility)
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=> {
+            nav.classList.toggle('show');     
+        })
+    }
+}
+
+showMenu("nav_toggle","nav_menu");
+
+
+// REMOVE MENU
+const navLink = document.querySelectorAll('.nav_link'),
+ nav_menu = document.getElementById('nav_menu')
+
+function linkAction(){
+    nav_menu.style.display = 'none';
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+// show mix list
+
+const showList = function () {
+    const showBtn = document.getElementById('mix_list');
+    const musicList = document.querySelector('.mix_list1');
+    const mixPlayerContent = document.querySelector('.mix_player_content');
+    const player = document.querySelector('.player')
+    showBtn.addEventListener('click', () => {
+        musicList.classList.toggle('mix_list1_show')
+                
+    })
+}
+
+showList();
