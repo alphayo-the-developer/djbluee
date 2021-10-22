5613dfb8645808e7bd4206a95320b402998b1651
alphayo-the-developer <alphayothedeveloper@gmail.com>
2021-10-22 14:13:18 +0300

    add music player

--- server.js
+++ server.js
@@ -11,18 +11,21 @@ app.get("/", function (req, res) {
 
 
 
-app.get("/video", function (req, res) {
+app.get("/audo", function (req, res) {
+    console.log(req.headers)
     const range = req.headers.range;
     if(!range) {
-        res.status(400).send("require range header");
+        // res.status(400).send("require range header");
     }
 
-    const videoPath = "./videos/gone.mp4";
-    const videoSize = fs.statSync("./videos/gone.mp4").size;
+    const videoPath = path.join(__dirname, 'music', "/ukulele.mp3")
+    // const videoPath = "public/videos/bluee_video.mp4"
+    const videoSize = fs.statSync(videoPath).size;
 
     //parse range
     const CHUNK_SIZE = 10**6; //1MB
-    const start = Number(range.replace(/\D/g, ""));
+    // const start = Number(range.replace(/\D/g, ""));
+    const start = Number(4);
     const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
 
     const contentLength = end - start + 1;
@@ -30,7 +33,7 @@ app.get("/video", function (req, res) {
         "Content-Range": `bytes ${start} - ${end}/${videoSize}`,
         "Accept-Ranges": "bytes",
         "Content-Length": contentLength,
-        "content-Type": "video/mp4",
+        "content-Type": "audio/mp3",
     };
 
     res.writeHead(206, headers);
