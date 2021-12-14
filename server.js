ec0f6e1082ce86dadb7454b71249b47fd0992dfc
alphayo ken <82713254+alphayo-the-developer@users.noreply.github.com>
2021-12-14 08:40:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require('path');
 const app = express();
-
+const port = process.env.PORT || 5000; 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function (req, res) {
@@ -75,4 +75,4 @@ app.get("/video", function (req, res) {
 })
 
 
-app.listen(5000);
+app.listen(port);
